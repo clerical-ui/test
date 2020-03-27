ffb5d869cb97b14737c44df83b781d1b02c89c77
Tom Manion <thomas.w.manion@accenture.com>
2020-03-27 15:09:28 -0500

    update structure

---
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Clerical Docs",
+        "excerpt":"The minimal code, Contract-Driven UI web component framework. What is a Contract-Driven UI? TODO Why is Clerical different? Most web application frameworks encourage and expect UI code to include all business logic and functionality to be bundled into a single application. These applications can become massive and complex. With Clerical,...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/docs/",
+        "teaser": null
+      },{
+        "title": "Quick-Start Guide",
+        "excerpt":"Hello World  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/docs/quick-start-guide/",
+        "teaser": null
+      },]
